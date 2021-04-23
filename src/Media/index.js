4a46b2b46175522ceadb34324a8822b976bc0034
GentritBegaj <gentritbegaj@gmail.com>
2021-04-23 21:38:14 +0200

    key added

--- src/Media/index.js
+++ src/Media/index.js
@@ -14,7 +14,7 @@ const cloudStorage = new CloudinaryStorage({
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY, //key
   api_secret: process.env.API_SECRET,
 });
 
